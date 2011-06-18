8bb269ecd29af677796310ccd72cd731a5acb0de
Myth <igor.myth@gmail.com>
2011-06-19 01:18:14 +0300

    Attacker was added

--- src/main.lisp
+++ src/main.lisp
@@ -49,7 +49,9 @@
         (debug-format ">>> ~A ~A ~A~%" slot-number amount tmp-slot)
         (make-task :commands-list
                     (compile-lambda
-                        `(progn (set ,tmp-slot (loop zz (help ',slot-number (K ',slot-number zz) ',amount)))
+                        `(progn (set ,tmp-slot (loop zz (help ',slot-number ,(if (= 0 slot-number)
+                                                                                 'zz
+                                                                                 `(K ',slot-number zz)) ',amount)))
                                 (set 0 (get ',tmp-slot))
                                 (set 0 (0 zero)))
                         :target-slot tmp-slot
@@ -57,6 +59,22 @@
                                                 (find-slots *proponent*
                                                             :score-fn (lambda (slot i) (when (dead? slot) 1)))))
                     :required-slots (list slot-number tmp-slot))))
+
+(defun make-attack-task (attacker-slot-number attacked-slot-number amount)
+    (destructuring-bind (tmp-slot) (find-slots *proponent* :slots-number 1
+                                                           :score-fn #'slot-score)
+        (make-task :commands-list
+                    (compile-lambda
+                        `(attack ',attacker-slot-number ',(- 255 attacked-slot-number) ',amount)
+                        :target-slot tmp-slot
+                        :prealloc-slots (append (list tmp-slot)
+                                                (find-slots *proponent*
+                                                            :score-fn (lambda (slot i) (when (dead? slot) 1)))))
+                    :required-slots (list tmp-slot attacker-slot-number)
+                    :complete-check-function (lambda () (let ((v (slot-vitality (get-slot *opponent* attacked-slot-number)))
+                                                              (v2 (slot-vitality (get-slot *proponent* attacker-slot-number))))
+                                                          (and (> v 0)
+                                                               (> v2 amount)))))))
 
 (defun find-slot-to-heal (player)
     (car (find-slots player :slots-number 1
@@ -66,20 +84,38 @@
                                             (when (> v 0)
                                                 (+ (/ v 100) d)))))))
 
+(defun find-slot-to-attack (player)
+    (car (find-slots player :slots-number 1
+                            :score-fn (lambda (slot index)
+                                        (let ((v (slot-vitality slot))
+                                              (d (num-difficulty index)))
+                                            (when (> v 0)
+                                                (+ (/ v 100) (- 16 d))))))))
+
 (defun find-attacker-slot (player)
     (car (find-slots player :slots-number 1
                             :score-fn (lambda (slot index)
                                         (let ((v (slot-vitality slot))
                                               (d (num-difficulty index)))
                                             (when (> v 0)
-                                                (+ (/ v 100) d)))))))
+                                                (+ (/ v 100) (- 16 d)))))
+                            :test-fn #'>)))
 
 
 (defun select-task ()
-    (let ((slot-to-heal (find-slot-to-heal *proponent*)))
-        (if slot-to-heal
-           (make-health-booster-task slot-to-heal (1- (slot-vitality (get-slot *proponent* slot-to-heal))))
-           (list 'left 'I 0))))
+    ;; (let ((slot-to-heal (find-slot-to-heal *proponent*)))
+    ;;     (if slot-to-heal
+    ;;        (make-health-booster-task slot-to-heal (1- (slot-vitality (get-slot *proponent* slot-to-heal))))
+    ;;        (list 'left 'I 0)))
+  (let* ((attacker (find-attacker-slot *proponent*))
+         (attacked (find-slot-to-attack *opponent*))
+         (attacker-v (slot-vitality (get-slot *proponent* attacker)))
+         (attacked-v (slot-vitality (get-slot *opponent* attacked))))
+    (if (and attacker attacked)
+        (make-attack-task attacker attacked (if (< attacker-v (truncate (* attacked-v 10/9)))
+                                                (truncate (* attacker-v 0.9))
+                                                attacked-v))
+        (list 'left 'I 0))))
 ;    (if *last-application*
 ;        (make-task :commands-list (list *last-application*))
 ;        (make-task :commands-list (list (list 'left 'I 0)))))
